be92e8acd5f312f35df057e4f267d6045d5c917c
wawstream <wawstream@gmail.com>
2025-09-05 17:04:03 +0300

    addTraining.js

---
+++ netlify/functions/addTraining.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async (event, context) => {
+  try {
+    const DATA_FILE = path.join(__dirname, '../../data.json');
+    
+    let data = { trainings: [], participants: [] };
+    if (fs.existsSync(DATA_FILE)) {
+      data = JSON.parse(fs.readFileSync(DATA_FILE));
+    }
+    
+    const { id, title, date, group, desc } = JSON.parse(event.body);
+    data.trainings.push({ id, title, date, group, desc });
+    
+    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ success: true })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ error: 'Server error' })
+    };
+  }
+};
