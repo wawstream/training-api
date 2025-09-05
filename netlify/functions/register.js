603c5a36878ded5e15b93fb9845ef0f2259c9297
wawstream <wawstream@gmail.com>
2025-09-05 17:03:43 +0300

    register.js

---
+++ netlify/functions/register.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async (event, context) => {
+  try {
+    const DATA_FILE = path.join(__dirname, '../../data.json');
+    
+    let data = { trainings: [], participants: [] };
+    if (fs.existsSync(DATA_FILE)) {
+      data = JSON.parse(fs.readFileSync(DATA_FILE));
+    }
+    
+    const { training_id, name } = JSON.parse(event.body);
+    data.participants.push({ training_id, name });
+    
+    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ success: true })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ error: 'Server error' })
+    };
+  }
+};
